c796448287df34b3f4e7caa048296d15d4773930
LAPTOP-FABIAN\Fabian <fabsterdanoontjedevries@gmail.com>
2017-12-05 10:59:17 +0100

    Michael fixed the reposetory

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Mantelcheck</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<nav>
+			<ul>
+				<li><a href="">Voeding</a></li>
+				<li><a href="">Valpreventie</a></li>
+				<li><a href="">Instanties</a></li>
+				<li><a href="">Mantelcheck</a></li>
+			</ul>
+		</nav>
+	</body>
+	<footer>
+		<p>©Copyright</p>
+	</footer>
+</html>
