d239da1f714bc3f61c5731aae2ddb060ef89c354
LAPTOP-FABIAN\Fabian <fabsterdanoontjedevries@gmail.com>
2017-12-06 11:21:43 +0100

    Home page front end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,6 +16,31 @@
 				<li><a href="">Mantelcheck</a></li>
 			</ul>
 		</nav>
+		<div id="cover">
+			<img id="cover_img" src="placeholder-image4.jpg" alt="missing"/>
+			<div id="image_text">
+				<h1>Bent u mantelzorger?</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacus nisl, maximus in nisi ut, hendrerit tristique nulla. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam fringilla luctus turpis, finibus interdum augue condimentum eu. Aliquam vitae urna auctor, euismod magna ut, aliquam est. Nam semper leo non ipsum pharetra pellentesque. Nulla porta pharetra libero, nec aliquet ligula eleifend id.</p>
+				<a id="image_button" href="">Knop</a>
+			</div>
+		</div>
+		<div id="flexbox_bodyblock">
+			<div class="bodyblock">
+				<h2>Bodyblock left</h2>
+				<img class="bodyblock_img" src="placeholder-image4.jpg" alt="missing"/>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacus nisl, maximus in nisi ut, hendrerit tristique nulla. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam fringilla luctus turpis, finibus interdum augue condimentum eu.</p>
+			</div>
+			<div class="bodyblock">
+				<h2>Bodyblock center</h2>
+				<img class="bodyblock_img" src="placeholder-image4.jpg" alt="missing"/>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacus nisl, maximus in nisi ut, hendrerit tristique nulla. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam fringilla luctus turpis, finibus interdum augue condimentum eu.</p>
+			</div>
+			<div class="bodyblock">
+				<h2>Bodyblock right</h2>
+				<img class="bodyblock_img" src="placeholder-image4.jpg" alt="missing"/>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacus nisl, maximus in nisi ut, hendrerit tristique nulla. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam fringilla luctus turpis, finibus interdum augue condimentum eu.</p>
+		</div>
+	</div>
 	</body>
 	<footer>
 		<p>©Copyright</p>
